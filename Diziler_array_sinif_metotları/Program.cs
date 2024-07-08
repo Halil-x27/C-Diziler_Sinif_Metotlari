d09b63aec8efcebb3ed1834bdf50bf4bd7042275
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-08 17:56:08 +0300

    Add files via upload

---
+++ Diziler_array_sinif_metotları/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Diziler_sinif_metotlari
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // sort
+
+            //sırasız sayi dizisi
+            int [] sayiDizisi = {13,23,7,4,16,39};
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //sirali sayi dizisi
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // Clear
+
+            // sayi dizisi elemanlarını kullanarak 2.indexten 2 tane elemanı  sıfırlar.
+
+            Array.Clear(sayiDizisi, 2,2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+
+            //Reverse  tersine çevirme
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }   
+
+
+            //İndexOf kaçıncı indexte olduğunu bulma
+ 
+            Array.IndexOf(sayiDizisi, 23);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            } 
+
+
+            // Resize boyutlandırma
+
+            Array.Resize<int>(ref sayiDizisi, 8);  
+            sayiDizisi[7] = 99;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }      
+            
+        }
+    }
+}
